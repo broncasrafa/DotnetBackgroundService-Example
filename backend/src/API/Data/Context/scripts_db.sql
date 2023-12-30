a00d8e3e4bc1915787843924618f260417281f84
Rafael Francisco <broncasrafa@gmail.com>
2023-12-30 00:33:48 -0300

    adicionado ao arquivo de script para criar o banco e as tabelas, a query para conferir as informações

--- backend/src/API/Data/Context/scripts_db.sql
+++ backend/src/API/Data/Context/scripts_db.sql
@@ -46,3 +46,29 @@ create table tbSolicitacao
 	CONSTRAINT [FK_tbSolicitacao_tbStatus_Processamento_2] FOREIGN KEY([IdStatusProcessamento_2]) REFERENCES tbStatus ([Id]),
 	CONSTRAINT [FK_tbSolicitacao_tbStatus_Processamento_3] FOREIGN KEY([IdStatusProcessamento_3]) REFERENCES tbStatus ([Id])
 )
+
+
+-- query para conferir
+SELECT
+	s.Id,
+	DataSolicitacao,
+	Documento,
+	IdUsuario,
+	ss.Descricao AS [Status_Solicitacao],
+	DataProcessamento_1_Inicio,
+	DataProcessamento_1_Fim,
+	ss1.Descricao AS [Status_Process_1],
+	DataProcessamento_2_Inicio,
+	DataProcessamento_2_Fim,
+	ss2.Descricao AS [Status_Process_2],
+	DataProcessamento_3_Inicio,
+	DataProcessamento_3_Fim,
+	ss3.Descricao AS [Status_Process_3]
+FROM tbSolicitacao (NOLOCK) s
+INNER JOIN tbStatus ss (NOLOCK) ON ss.Id = s.IdStatus
+INNER JOIN tbStatus ss1 (NOLOCK) ON ss1.Id = s.IdStatusProcessamento_1
+LEFT JOIN tbStatus ss2 (NOLOCK) ON ss2.Id = s.IdStatusProcessamento_2
+LEFT JOIN tbStatus ss3 (NOLOCK) ON ss3.Id = s.IdStatusProcessamento_3
+WHERE 1 = 1
+AND cast(DataSolicitacao as date) = cast(getdate() as date)
+ORDER BY s.Id DESC
